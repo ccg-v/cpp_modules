f3fa901843b9c3143720aac15fe07ef5df732c41
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2024-03-24 22:20:21 +0100

    Back to previous files: one per class, same upperCamelCase naming as classes

--- cpp_00/ex01/sources/Contact.cpp
+++ cpp_00/ex01/sources/Contact.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   add_contact.cpp                                    :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 20:02:02 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/03/24 21:28:16 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/03/24 22:15:43 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static bool hasOnlyBlankSpaces(const std::string &str);
 static bool areAllDigits(const std::string &str);
 static std::string trimBlankSpaces(const std::string &str);
 static void getContactDetails(Contact &newContact);
+static std::string formatAndTruncate(const std::string &str);
 
 void PhoneBook::addContact() {
 
@@ -88,6 +89,66 @@ void getContactDetails(Contact &newContact) {
 }
 
 /*
+ *	searchContact()
+ *
+ *	1. Warn if search is not possible because there no contacts were added yet
+ *	2. If possible, display the list of contacts formatted as subject demands:
+ * 		- a list header separated by pipes
+ *		- each contact in a new line
+ *		- the width of each columnn must be 10 characters, if the field value is
+ *			longer it must be shown truncated and last character replaced by '.'
+ *	3. Prompt the user for the index of the contact to display
+ *		- check if the input is valid (from 0 to 7)
+ *	4. Display the information of the selected contact
+ */
+
+void PhoneBook::searchContact() {
+	
+    if (currentIndex == 0) {
+        std::cout << "Phonebook is empty. No contacts to search." << std::endl;
+        return ;
+    }
+
+    std::cout << std::setw(10) << "Index" << "|"
+              << std::setw(10) << "First Name" << "|"
+              << std::setw(10) << "Last Name" << "|"
+              << std::setw(10) << "Nickname" << "|" << std::endl;
+
+    for (int i = 0; i < currentIndex; ++i) {
+        std::cout << std::setw(10) << i << "|"
+                  << std::setw(10) << formatAndTruncate(contacts[i].getFirstName()) << "|"
+                  << std::setw(10) << formatAndTruncate(contacts[i].getLastName()) << "|"
+                  << std::setw(10) << formatAndTruncate(contacts[i].getNickname()) << "|" << std::endl;
+    }
+
+	int index;
+    bool validIndex = false;
+    do {
+        std::cout << "Enter the index of the contact to display: ";
+		// std::cin >> index; // Getting index value here causes strange behavior
+
+        if (!(std::cin >> index) || index < 0 || index >= currentIndex) {
+            // Clear the error state and discard invalid input (see note below)
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid index. Please try again." << std::endl;
+            validIndex = false;
+        } else {
+            validIndex = true;
+        }
+    } while (!validIndex);
+
+    std::cout << std::endl << "Contact number " << index << " information:" << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << "First Name: " << contacts[index].getFirstName() << std::endl;
+    std::cout << "Last Name: " << contacts[index].getLastName() << std::endl;
+    std::cout << "Nickname: " << contacts[index].getNickname() << std::endl;
+    std::cout << "Phone Number: " << contacts[index].getPhoneNumber() << std::endl;
+    std::cout << "Darkest Secret: " << contacts[index].getDarkestSecret() << std::endl;
+
+}
+
+/*
  * hasOnlyBlankSpaces()
  *
  * Returns true if the input has only blank spaces.
@@ -141,7 +202,11 @@ bool areAllDigits(const std::string &str) {
 	return true;
 }
 
-// Trim leading and trailing blank spaces in the fields values
+/*
+ * trimBlankSpaces()
+ *
+ * Trims leading and trailing blank spaces in the fields values
+ */
 std::string trimBlankSpaces(const std::string &str) {
     int start = 0, end = str.length() - 1;
 
@@ -150,6 +215,19 @@ std::string trimBlankSpaces(const std::string &str) {
     while (end >= start && str[end] == ' ')
         end--;
     return str.substr(start, end - start + 1);
+}
+
+/*
+ * formatAndTruncate()
+ *
+ * Truncate and format the string if it is longer than the column width
+ */ 
+std::string formatAndTruncate(const std::string &str) {
+    if (str.length() > 10) {
+        return str.substr(0, 9) + ".";
+    } else {
+        return str;
+    }
 }
 
 /*
