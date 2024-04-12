3d7d8e7dc2215f023c48e624cc85bfda5cde3117
Carlos <ccg-v@hotmail.com>
2024-04-12 18:43:16 +0200

    Still working on naming new file

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:58:32 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/04/12 00:28:41 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/04/12 13:42:07 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,40 @@ int	main(int argc, char **argv) {
 	
 	if (argc != 4) {
 		std::cout	<< "Wrong arguments!" << std::endl 
-					<< "Usage: <executable> <filename> <string to replace> <new string>" 
+					<< "Usage: "
+					<< argv[0] 
+					<< " <filename> <string to replace> <new string>" 
 					<< std::endl;
 		return 1;
 	}
 
-	std::ifstream ifs;
-	ifs.open (argv[1], std::ifstream::in);
-	if (!ifs.is_open()) {
+	std::ifstream inputFile;
+	inputFile.open (argv[1], std::ifstream::in);
+	if (!inputFile.is_open()) {
 		std::cout << "Error opening file" << std::endl;
 		return 1;
 	}
+
+	std::string	fileName = argv[1];
+	std::string str1 = argv[2];
+	std::string	str2 = argv[3];
+
+	std::ofstream outputFile;
+	std::size_t found = fileName.find(".");
+	if (found == std::string::npos)
+		std::string outputFileName = (fileName + ".replace");
+	else
+		std::string outputFileName = fileName.substr(0, found) + ".replace";
 	
+
+
+	
+
+
+	// while (!inputFile.eof()) {
+	// 	std::cout << getline()
+
+	// }
 	
 	return 0;
 }
