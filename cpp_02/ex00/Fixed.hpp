41abd39ce99554742ac34ca5ce0ad716433f4360
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2024-04-28 13:51:19 +0200

    _raw variable clarified

--- cpp_02/ex00/Fixed.hpp
+++ cpp_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 13:04:54 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/04/26 19:49:31 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/04/28 13:41:10 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Fixed {
 
 	private:
-		int					_raw;
+		int					_raw;	// (1)
 		static const int	_fractionalBits = 8;
 	
 	public:
@@ -46,3 +46,18 @@ class Fixed {
 };
 
 #endif
+
+/*
+ * (1) 	Although '_raw' is declared as an integer, it stores indeed the entire 
+ *		representation of the fixed-point number after conversion.
+ *
+ *		In fixed-point arithmetic, a fixed-point number is represented using 
+ *		an integer type, where a portion of the integer bits is reserved for 
+ *		the integer part of the number, and the remaining bits are used for 
+ *		the fractional part.
+ *		
+ *		So, even though _raw is declared as an integer, it effectively represents 
+ *		a fixed-point number. The integer bits of _raw represent the integer part 
+ *		of the fixed-point number, and the fractional bits (determined by the 
+ *		'_fractionalBits' constant) represent the fractional part.
+ */
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 22:47:54 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/04/28 10:16:35 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/04/28 13:40:28 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Fixed {
 
 	private:
-		int					_raw;
+		int					_raw;	// (1)) 
 		static const int	_fractionalBits = 8;
 
 	public:
@@ -56,3 +56,18 @@ class Fixed {
 std::ostream& operator<<( std::ostream& os, const Fixed& fixed_nbr );
 
 #endif
+
+/*
+ * (1) 	Although '_raw' is declared as an integer, it stores indeed the entire 
+ *		representation of the fixed-point number after conversion.
+ *
+ *		In fixed-point arithmetic, a fixed-point number is represented using 
+ *		an integer type, where a portion of the integer bits is reserved for 
+ *		the integer part of the number, and the remaining bits are used for 
+ *		the fractional part.
+ *		
+ *		So, even though _raw is declared as an integer, it effectively represents 
+ *		a fixed-point number. The integer bits of _raw represent the integer part 
+ *		of the fixed-point number, and the fractional bits (determined by the 
+ *		'_fractionalBits' constant) represent the fractional part.
+ */
