ef26e64de0bac3f823ee1fb60a42249ce6964b8d
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2024-04-28 12:24:39 +0200

    Instructions added to test script

--- cpp_02/ex01/test.sh
+++ cpp_02/ex01/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Before running copy the expected output from the subject to
+# a new file called 'expected_output'. 
+# Don't forget checking the content is copied with the proper
+# indentation!
+
 # Check if 'expected_output.txt' exists
 if [ ! -e "expected_output.txt" ]; then
     echo "Error: 'expected_output.txt' not found"
