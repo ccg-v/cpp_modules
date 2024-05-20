bd43d891a217f932764eedf27ac3fccad2ea7154
Carlos Carracedo garcia-villalba <ccg-v@hotmail.com>
2024-05-20 14:02:45 +0200

    cpp_04 - ex01 started

--- cpp_04/ex01/sources/Brain.cpp
+++ cpp_04/ex01/sources/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 13:29:55 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/05/20 13:41:30 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:02:30 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /* --- Orthodox Canonical Form ---------------------------------------------- */
 
 //	Default constructor
-Brain::Brain( void ) {
+Brain::Brain( void ) : {
 	std::cout << "[Brain Default Constructor called]" << std::endl;
 };
 
