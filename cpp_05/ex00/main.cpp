0cd43fa4b15beeb3e2b2845bb4f92218fd2e5c03
Carlos <ccg-v@hotmail.com>
2024-07-18 18:08:52 +0200

    Working on main

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 22:05:54 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/07/18 00:24:40 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/07/18 14:11:09 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 int main() {
 
-	// Bureaucrat b("Gregor", 200);
+	Bureaucrat b("Gregor", 200);
     // b.incrementGrade(100);
 	// Bureaucrat b();
 	try {
 		// Bureaucrat b();
-	 	Bureaucrat b("Gregor", 100);        
-		b.incrementGrade(100); // This will throw an exception
+	 	// Bureaucrat b("Gregor", 100);        
+		b.incrementGrade(1); // This will throw an exception
 		std::cout << b.getName() << " is grade " << b.getGrade() << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
+		std::cout << b.getName() << " is grade " << b.getGrade() << std::endl;
 	}
 	// std::cout << b.getName() << " is grade " << b.getGrade() << std::endl;
     return 0;
