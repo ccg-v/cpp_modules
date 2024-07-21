0658f147445b1fcdd03bf75aa03c97a5a1146df8
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-07-22 00:19:11 +0200

    cpp_05 - ex02: creating derived classes

---
+++ cpp_05/ex02/includes/PresidentialPardonForm.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 21:24:59 by ccarrace          #+#    #+#             */
+/*   Updated: 2024/07/21 22:14:54 by ccarrace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class	PresidentialPardonForm : public AForm {
+
+};
+
+#endif
---
+++ cpp_05/ex02/includes/ShrubberyCreationForm.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 21:25:04 by ccarrace          #+#    #+#             */
+/*   Updated: 2024/07/21 23:41:43 by ccarrace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm {
+
+	private:
+		std::string _target;
+
+	public:
+
+};
+
+#endif
+
