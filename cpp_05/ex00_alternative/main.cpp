f166624c163b2b28b1be59b7c0ab7742cb4fbdc3
Carlos <ccg-v@hotmail.com>
2024-07-31 13:05:11 +0200

    Fully working, pros and cons of version explained

--- cpp_05/ex00_alternative/main.cpp
+++ cpp_05/ex00_alternative/main.cpp
@@ -6,9 +6,38 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 22:05:54 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/07/31 10:43:13 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/07/31 12:59:28 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+ * In this version, the forest of try-catch blocks has been removed from the 
+ * main and placed within the parameterized constructor. This approach isn't
+ * wrong, inefficient, or unsafe per se. It's a valid design choice. However,
+ * it might lead to some issues:
+ *
+ *  - Lack of feedback on failure: 
+ * 		When an exception is caught within the constructor, the object is still 
+ * 		created, but it might not be in a valid state. This can be confusing as 
+ * 		users of the class might expect the object to be either fully valid or 
+ * 		not created at all.
+ *
+ *	- Inconsistent state: 
+ *		If the constructor catches an exception and logs an error, the object 
+ *		still exists but may not have the intended state. For example, an object 
+ *		might be created with a default grade if the provided grade was invalid,
+ *		but this can lead to unpredictable behavior.
+ *
+ *	- Separation of concerns:
+ *		It's often clearer to handle exceptions outside the class, where the 
+ *		context of object creation is better understood, and appropriate actions
+ *		can be taken based on the specific situation.
+ *
+ *  It's often better to let the exception propagate out of the constructor.
+ * 	This way, if object creation fails due to an invalid grade, the calling 
+ * 	code can handle it appropriately (e.g., by retrying with a different grade,
+ * 	logging the error, or informing the user).
+ */
 
 #include "Bureaucrat.hpp"
 
@@ -24,7 +53,7 @@ int main() {
 	<< "/* --- Creating a bureaucrat --------------------------------------- */"
 	<< std::endl << std::endl;
 
-	grade = 100;
+	grade = 200;
 	Bureaucrat a("Bob", grade);
 	
 	std::cout << a << std::endl;
@@ -40,8 +69,9 @@ int main() {
 	Bureaucrat b("Carol", grade);
 
 	std::cout << b << std::endl;
-	std::cout << "\t" << b.getName() << " has just been upgraded!" << std::endl;
-	a.incrementGrade(increment);
+	std::cout << "\t" << b.getName() << " is to be promoted " << increment
+			  << " grades" << std::endl;
+	b.incrementGrade(increment);
 	std::cout << b << std::endl;
 
 //	Object downgrading
@@ -55,8 +85,9 @@ int main() {
 	Bureaucrat c("Ted", grade);
 
 	std::cout << c << std::endl;
-	std::cout << "\t" << c.getName() << " has just been downgraded!" << std::endl;
-	a.decrementGrade(decrement);
+	std::cout << "\t" << b.getName() << " is to be downgraded " << decrement
+			  << " grades" << std::endl;
+	c.decrementGrade(decrement);
 	std::cout << c << std::endl;
 
 //	Object instantiated with wrong grade (should throw an exception)
