1b26a6a931c23b21e5555ad8c77e52e01821cd69
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-08-20 02:29:14 +0200

    Swap() refactored to follow subject's main()/ Adding comments on this issue

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -6,25 +6,27 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 22:57:47 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/08/20 00:53:27 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/08/20 02:05:19 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
+#include <limits>	// std::numeric_limits
+
 
 template <typename T>
 void test(T value1, T value2) {
 
 	std::cout << "Before swapping:\tvalue1 = " << value1
 			  << "\tvalue2 = " << value2 << std::endl;	
-	Swap(&value1, &value2);
+	swap(value1, value2);
 	std::cout << "After swapping :\tvalue1 = " << value1
 			  << "\tvalue2 = " << value2 << std::endl;
 
-	std::cout << "Min value is " << Min(value1, value2) << std::endl; 
-	std::cout << "Max value is " << Max(value1, value2) << std::endl;
-	std::cout << std::endl;	
+	std::cout << "Min value is " << min(value1, value2) << std::endl; 
+	std::cout << "Max value is " << max(value1, value2) << std::endl;
+	std::cout << std::endl;
 }
 
 int	main(void)
@@ -32,34 +34,44 @@ int	main(void)
 	std::cout << "/* --- Test with integer values ------------------------ */\n"
 			  << std::endl;
 
-	int	int1 = 1;
-	int int2 = 200;
-
-	test(int1, int2);
+	test(1, 200);
+	test(std::numeric_limits<int>::max(), std::numeric_limits<int>::min());
 
 	std::cout << "/* --- Test with float values -------------------------- */\n"
 			  << std::endl;
 
-	float float1 = 42.0f;
-	float float2 = 365.99f;
+	test(42.0f, 365.99f);
+	test(std::numeric_limits<float>::max(), std::numeric_limits<float>::min());
 
-	test(float1, float2);
 
 	std::cout << "/* --- Test with char values --------------------------- */\n"
 			  << std::endl;
 
-	char char1 = 'Z';
-	char char2 = 'a';
-
-	test(char1, char2);
+	test('Z', 'a');
 
 	std::cout << "/* --- Test with strings ------------------------------- */\n"
 			  << std::endl;
-			  
-	std::string str1 = "apple";
-	std::string str2 = "banana";
 
-	test(str1, str2);
+	test("prime", "prize");
 
 	return 0;
 }
+
+// int main( void ) {
+// 	int a = 2;
+// 	int b = 3;
+
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+// 	std::string c = "chaine1";
+// 	std::string d = "chaine2";
+	
+// 	::swap(c, d);
+// 	std::cout << "c = " << c << ", d = " << d << std::endl;
+// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+// 	return 0;
+// }
--- cpp_07/ex00/whatever.hpp
+++ cpp_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 22:41:33 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/08/20 00:29:16 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/08/20 02:26:33 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,40 @@
 #include <iostream>
 
 template <typename T>
-void Swap(T *a, T *b)
+void swap(T &a, T &b)	// (1)
 {	
 	T tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
 }
 
+// template <typename T>
+// void swap(T *a, T *b)
+// {	
+// 	T tmp;
+
+// 	tmp = *a;
+// 	*a = *b;
+// 	*b = tmp;
+// }
+
 template <typename T>
-T Min(T a, T b)
+T min(T a, T b)
 {
 	return (a < b ? a : b);
 }
 
 template <typename T>
-T Max(T a, T b)
+T max(T a, T b)
 {
 	return (a > b ? a : b);
 }
 
 #endif
+
+/*
+ *	(1) Notice that this implementation would not work in C because C does not 
+ *		support references (`T &a`, `T & b`) like C++ does.
+ */
