266b25c2e7e5ab508f2d32f4169af7815c37493f
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-08-21 00:37:29 +0200

    Starting ex01

---
+++ cpp_07/ex01/iter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 19:05:01 by ccarrace          #+#    #+#             */
+/*   Updated: 2024/08/20 20:56:39 by ccarrace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <cstddef>
+
+template <typename T>
+void iter (T*, size_t length, void(*funct)(T)) {
+
+};
+
+#endif
