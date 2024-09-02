82fbbebb59ad268b0003572811c34c6cfb55b7e5
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-09-02 02:07:47 +0200

    About to add array of pointers and 2d array tests

--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 21:07:46 by carlos            #+#    #+#             */
-/*   Updated: 2024/09/01 23:33:29 by carlos           ###   ########.fr       */
+/*   Updated: 2024/09/02 02:05:55 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,13 @@
 //     num *= num;
 // }
 
-// Function to convert string to uppercase
-void toUpperCase(std::string & str) {
-    for (size_t i = 0; i < str.length(); ++i) {
-        str[i] = std::toupper(static_cast<unsigned char>(str[i]));
-    }
-}
+// // Function to convert string to uppercase
+// void toLowerCase(std::string & str) {
+//     for (size_t i = 0; i < str.length(); ++i) {
+//         str[i] = std::tolower(static_cast<unsigned char>(str[i]));
+//     }
+// }
+
 
 // // Function to print an element
 // template <typename T>
@@ -48,9 +49,23 @@ void incrementElement(T & element) {
 
 // Specialization for the Point struct
 template <>
-void incrementElement(Point& p) {
+void incrementElement(Point & p) {
     p.x += 42;
     p.y += 42;
+}
+
+// Function to convert string to uppercase
+template <typename T>
+void toUpperCase(T & str) {
+    for (size_t i = 0; i < str.length(); ++i) {
+        str[i] = std::toupper(static_cast<unsigned char>(str[i]));
+    }
+}
+
+// Specialization of toUpperCase for single chars
+template <>
+void toUpperCase(char & c) {
+    c = std::toupper(static_cast<unsigned char>(c));
 }
 
 // Function to print an element
@@ -75,103 +90,104 @@ void printElement(const T& element) {
 
 int main() {
 
-    /* --- Test 1: Double each element ------------------------------------- */
+    /* --- Test 1: Increment each element ---------------------------------- */
 
     std::cout << "/* ---- Test 1: Increment each element ------------------- */"
               << std::endl << std::endl;
+
+    //  Int array
     int intArr[] = {1, 2, 3, 4, 5};
     size_t length = sizeof(intArr) / sizeof(intArr[0]);
     
-    // printArray("Original   : {", intArr, length);
-    std::cout << "\tInitial int array  : ";
+    std::cout << "\t// Int array" << std::endl;
+    std::cout << "\tOriginal   : ";
     iter(intArr, length, printElement<int>);
     std::cout << std::endl;
     iter(intArr, length, incrementElement<int>);
-    std::cout << "\tResulting int array: ";
-    iter(intArr, length, printElement<int>);
-    // printArray("Transformed: {", intArr, length);    
+    std::cout << "\tTransformed: ";
+    iter(intArr, length, printElement<int>);  
     std::cout << "\n" << std::endl;
 
-    // std::cout << "/* ---- Test 2: Double each element - Float array -------- */"
-    //           << std::endl << std::endl;
+    // Float array
     float floatArr[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
     length = sizeof(floatArr) / sizeof(floatArr[0]);
     
-    // printArray("Original   : {", floatArr, length);
-    std::cout << "\tInitial float array  : ";
+    std::cout << "\t// Float array" << std::endl;
+    std::cout << "\tOriginal   : ";
     iter(floatArr, length, printElement<float>);
     std::cout << std::endl;
     iter(floatArr, length, incrementElement<float>);
-    std::cout << "\tResulting float array: ";    
-    iter(floatArr, length, printElement<float>);
-    // printArray("Transformed: {", floatArr, length);    
+    std::cout << "\tTransformed: ";   
+    iter(floatArr, length, printElement<float>);  
     std::cout << "\n" << std::endl;
 
-    // /* --- Test 3 ---------------------------------------------------------- */
-
-    // std::cout << "/* ---- Test 3: Double each element - Double array ------- */" 
-    //           << std::endl << std::endl;
+    // Double array
     double doubleArr[] = {1.5, 2.5, 3.5, 4.5, 5.5};
     length = sizeof(doubleArr) / sizeof(doubleArr[0]);
 
-    // printArray("Original   : {", doubleArr, length);
-    std::cout << "\tInitial double array  : ";
+    std::cout << "\t// Double array" << std::endl;
+    std::cout << "\tOriginal   : ";
     iter(doubleArr, length, printElement<double>);
     std::cout << std::endl;
     iter(doubleArr, length, incrementElement<double>);
-    std::cout << "\tResulting double array: "; 
-    iter(doubleArr, length, printElement<double>);
-    // printArray("Transformed: {", doubleArr, length);    
+    std::cout << "\tTransformed: ";  
+    iter(doubleArr, length, printElement<double>);   
     std::cout << "\n" << std::endl;
 
-    // /* --- Test 4 ---------------------------------------------------------- */
-
-    // std::cout << "/* ---- Test 4: Increment each element - Char array ------ */" 
-    //           << std::endl << std::endl;
-    int charArr[] = {'A', 'B', 'C', 'D', 'E'};
+    // Char array
+    char charArr[] = {'A', 'B', 'C', 'D', 'E'};
     length = sizeof(charArr) / sizeof(charArr[0]);
 
-    // printArray("Original   : {", doubleArr, length);
-    std::cout << "\tInitial char array  : ";
+    std::cout << "\t// Char array" << std::endl;
+    std::cout << "\tOriginal   : ";
     iter(charArr, length, printElement<char>);
     std::cout << std::endl;
-    iter(charArr, length, incrementElement<int>);
-    std::cout << "\tResulting char array: "; 
-    iter(charArr, length, printElement<char>);
-    // printArray("Transformed: {", doubleArr, length);    
+    iter(charArr, length, incrementElement<char>);
+    std::cout << "\tTransformed: ";  
+    iter(charArr, length, printElement<char>); 
     std::cout << "\n" << std::endl;
 
-    // /* --- Test 6 ---------------------------------------------------------- */
-
-    // std::cout << "/* ---- Test 6: Increment elements - Struct array -------- */"
-    //           << std::endl << std::endl;
+    // Struct array
     Point pointArr[] = {{1, 2}, {3, 4}, {5, 6}};
     length = sizeof(pointArr) / sizeof(pointArr[0]);
     
-    std::cout << "\tInitial struct array  : ";
+    std::cout << "\t// Struct array" << std::endl;
+    std::cout << "\tOriginal   : ";
     iter(pointArr, length, printElement<Point>);
     std::cout << std::endl;
     iter(pointArr, length, incrementElement<Point>);
-    std::cout << "\tResulting struct array: "; 
+    std::cout << "\tTransformed: "; 
     iter(pointArr, length, printElement<Point>);
-    // printArray("Transformed: {", pointArr, length);
     std::cout << "\n" << std::endl;
 
     /* --- Test 2 ---------------------------------------------------------- */
 
-    std::cout << "/* ---- Test 2: Convert to uppercase - String array ------ */"
+    std::cout << "/* ---- Test 2: Convert to uppercase --------------------- */"
               << std::endl << std::endl;
+
+    // String array
     std::string strArr[] = {"cpp module 07", "exercise 01", "testing"};
     length = sizeof(strArr) / sizeof(strArr[0]);
 
-    // printArray("Original   : {", strArr, length);
+    std::cout << "\t// String array" << std::endl;
     std::cout << "\tOriginal   : ";
     iter(strArr, length, printElement<std::string>);
     std::cout << std::endl;
-    iter(strArr, length, toUpperCase);
+    iter(strArr, length, toUpperCase<std::string>);
     std::cout << "\tTransformed: "; 
     iter(strArr, length, printElement<std::string>);
-    // printArray("Transformed: {", strArr, length);
+    std::cout << "\n" << std::endl;
+
+    // Char array (I use the array of chars defined and transformed before)
+    length = sizeof(charArr) / sizeof(charArr[0]);
+    
+    std::cout << "\t// Char array" << std::endl;
+    std::cout << "\tOriginal   : ";
+    iter(charArr, length, printElement<char>);
+    std::cout << std::endl;
+    iter(charArr, length, toUpperCase<char>);
+    std::cout << "\tTransformed: ";  
+    iter(charArr, length, printElement<char>); 
     std::cout << "\n" << std::endl;
 
     /* --- Test 3 ---------------------------------------------------------- */
@@ -188,23 +204,6 @@ int main() {
     std::cout << "\tResulting  : "; 
     iter(emptyArr, length, printElement<int>);
     std::cout << "\n" << std::endl;
-    
-    // /* --- Test 8 ---------------------------------------------------------- */
-
-    // std::cout << "/* ---- Test 8: Double elements - Large Array ------------ */"
-    //           << std::endl << std::endl;
-    // int largeArr[100];
-    // for (int i = 0; i < 100; ++i) {
-    //     largeArr[i] = i;
-    // }
-
-    // std::cout << "\tOriginal   : ";  
-    // iter(largeArr, 100, printElement<int>);
-    // std::cout << std::endl;
-    // iter(largeArr, 100, doubleElement<int>);
-    // std::cout << "\tTransformed: ";    
-    // iter(largeArr, 100, printElement<int>);
-    // std::cout << "\n" << std::endl;
 
     return 0;
 }
