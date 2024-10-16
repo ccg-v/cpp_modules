567701757158c5703a6c52a0d209ba2b244d57fe
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-10-16 14:29:40 +0200

    Cleaning/adding comments

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 19:15:05 by ccarrace          #+#    #+#             */
-/*   Updated: 2024/10/11 20:06:57 by ccarrace         ###   ########.fr       */
+/*   Updated: 2024/10/16 14:26:37 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 # define EASYFIND_HPP
 
 # include <algorithm>	// for std::find
-# include <vector>		// for std:vector
-# include <deque>		// for std::deque
-# include <list>		// for std::list
 # include <stdexcept>   // for std::runtime_error
 # include <iostream>	// for std::ostream and std::cout
 # include <sstream> 	// For std::ostringstream
+# include <vector>		// for std:vector
+# include <deque>		// for std::deque
+# include <list>		// for std::list
 
+/* --- easyfind() function -------------------------------------------------- */
+
+// Generic function
 template <typename T>
 typename T::const_iterator easyfind(const T& container, int value); 
 
@@ -28,11 +31,18 @@ typename T::const_iterator easyfind(const T& container, int value);
 template <typename T, size_t N>
 T* easyfind(T (&array)[N], int value);
 
+/* --- Printing functions --------------------------------------------------- */
+
 // Generic function to print container contents
 template <typename T>
 void printContainer(const T& container);
 
-// Exception
+// Specialization to print C-style arrays
+template <typename T, size_t N>
+void printContainer(T (&array)[N]);
+
+/* --- Exceptions ----------------------------------------------------------- */ // (1)
+
 // class NoOccurrenceFoundException : public std::exception {
 // 	public:
 // 		const char *what() const throw();
@@ -41,3 +51,9 @@ void printContainer(const T& container);
 #include "easyfind.tpp"
 
 #endif
+
+/*
+ *	(1) I finally decided to throw a standard exception instead of a custom one. 
+ *		Read why here:
+ *		https://github.com/ccg-v/cpp_modules/tree/master/cpp_08/ex00#-throw-a-standard-or-a-custom-exception-
+ */
