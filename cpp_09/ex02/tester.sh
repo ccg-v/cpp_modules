6614f2c8668bd1a3ec65490fbedba7c52e683555
Carlos Carracedo garcía-villalba <ccg-v@hotmail.com>
2024-11-13 18:19:11 +0100

    Makefile modified to organize files in src/inc folders

--- cpp_09/ex02/tester.sh
+++ cpp_09/ex02/tester.sh
@@ -2,8 +2,8 @@
 # Set 'MAX' with the amount of numbers to be sorted
 # To run: ./tester.sh
 
-MAX=3000
+MAX=20
 NUMS=$(shuf -i 1-$MAX | tr '\n' ' ')
 
 echo "Generated sequence: $NUMS"
-./a.out $NUMS | wc -l
+./PmergeMe $NUMS | wc -l
